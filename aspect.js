a2f532dc796dd61821d45d1b028014404a7c8d95
Esteban Felipe <esteban.felipe@gmail.com>
2018-04-19 13:22:49 -0400

    Fixes

--- aspect.js
+++ aspect.js
@@ -11,7 +11,7 @@ module.exports = function(app){
       res.status(200).send(response);
       });
   app.post('/aspect/taskClose',(req,res) => {
-    var response = "<soapenv:Envelope xmlns:soapenv=\"http:\/\/schemas.xmlsoap.org\/soap\/envelope\/\" xmlns:tem=\"http:\/\/tempuri.org\/\" xmlns:eg=\"http:\/\/schemas.datacontract.org\/2004\/07\/eg.WorkManager.Framework.DTO\"><soapenv:Header\/><soapenv:Body><tem:egTaskEndResponse \/><\/tem:egTaskEndResult><\/tem:egTaskEndResponse><\/soapenv:Body><\/soapenv:Envelope>"
+    var response = "<soapenv:Envelope xmlns:soapenv=\"http:\/\/schemas.xmlsoap.org\/soap\/envelope\/\" xmlns:tem=\"http:\/\/tempuri.org\/\" xmlns:eg=\"http:\/\/schemas.datacontract.org\/2004\/07\/eg.WorkManager.Framework.DTO\"><soapenv:Header\/><soapenv:Body><tem:egTaskEndResponse \/><tem:egTaskEndResult \/><\/tem:egTaskEndResponse><\/soapenv:Body><\/soapenv:Envelope>"
       res.contentType("text/xml");
       res.status(200).send(response);
       });
